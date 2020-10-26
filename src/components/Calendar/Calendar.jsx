76f41528c45181b3bff8ac62dd133203deb8c83e
Johnny-Oleg <neojin.entity@gmail.com>
2020-10-26 22:00:33 +0300

    completed

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -23,7 +23,7 @@ const Calendar = () => {
                                     <div className="launches-content">
                                         <h2 className="launches-title">{item.name}</h2>
                                         <Link to={`/details/${item.id}`} className="button launches-details">
-                                            Подробнее
+                                            Details
                                         </Link>
                                     </div>
                                 </article>
